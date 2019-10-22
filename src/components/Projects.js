5030506f6c2af8f37da2412505c9c6b65306cb1c
Cassandra Terry <cassandra34terry@gmail.com>
2019-10-22 06:19:26 -0500

    project addition

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,42 +6,46 @@ class Projects extends Component {
         return (
             <div id="projects-container">
                 <Nav />
-                {/* <img id="flyingparrot-projects" src="/images/accessory.png" alt="about-flying"/> */}
                 <div className="projects">
-                    <img className="project-img" src="/images/mayan.png" alt="mayan-pic"/>
+                <a href="http://ec2-13-58-98-246.us-east-2.compute.amazonaws.com/"><img className="project-img" src="/images/dory.png" alt="dory-pic"/></a>
+
                     <div className="project-info">
-                        <a href="https://mayan-god-attack.herokuapp.com"><p className="project-name">Mayan God Attack</p></a>
-                        <p>Single player dodge and shoot game.</p>
+                        <a href="http://ec2-13-58-98-246.us-east-2.compute.amazonaws.com/"><p className="project-name">Defending Dory</p></a>
+                        <p>Single player dodge game. (Desktop Only)</p>
                         <p>Sole Developer.</p>
-                        <p>Technologies: Phaser.js and Express</p>
+                        <p>Technologies: Javascript, Phaser 3 (HTML5 game framework) and Express</p>
                     </div>
                 </div>
                 <div className="projects">
-                    <img className="project-img" src="/images/pixalive.png" alt="pixalive-pic"/>
+                <a href="https://pixalive.herokuapp.com"><img className="project-img" src="/images/pixalive.png" alt="pixalive-pic"/></a>
+
                     <div className="project-info">
                         <a href="https://pixalive.herokuapp.com"><p className="project-name">Pixalive</p></a>
                         <p>Multi-user, pixel art editor used to create pixel art and
-                            animiations. Try your hand at it and save as a gif.</p>
+                            animiations. Try your hand at it and save as a gif. (Desktop Only)</p>
                         <p>Fullstack Developer.</p>
                         <p>Technologies:  HTML Canvas, React Hooks and Websockets</p>
                     </div>
                 </div>
                 <div className="projects">
-                    <img className="project-img" src="/images/site.png" alt="site-pic"/>
+                <a href="https://cassandraterry-793fc.firebaseapp.com"><img className="project-img" src="/images/site.png" alt="site-pic"/></a>
+
                     <div className="project-info">
-                        <a href="http://www.cassandraterry.com.s3-website.us-east-2.amazonaws.com"><p className="project-name">Portfolio</p></a>
+                        <a href="https://cassandraterry-793fc.firebaseapp.com"><p className="project-name">Portfolio</p></a>
                         <p>React web application </p>
                         <p>Sole Developer.</p>
                         <p>Technologies:  React, HTML, CSS </p>
                     </div>
                 </div>
                 <div className="projects">
-                    <img className="project-img" src="/images/candy.png" alt="candy-pic"/>
+                <a href="https://international-candy.herokuapp.com">
+                <img className="project-img" src="/images/candy.png" alt="candy-pic"/></a>
+
                     <div className="project-info">
                         <a href="https://international-candy.herokuapp.com">
                             <p className="project-name">International Candy</p></a>
                         <p>E-commerce website with Passport and OAuth integration.<br />
-                             Styling not so great, but it works. :)</p>
+                             Styling not so great, but it works. :) (Desktop Only)</p>
                         <p>Fullstack Developer.</p>
                         <p>Technologies:  React, Redux, Express, PostgreSQL.</p>
                     </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,65 +7,66 @@ class Skills extends Component {
             <div id="skills-container">
                 <Nav />
                 <div id="skills">
-                    <div className="skills-row">
-                        <div className="skills-wrap">
-                            <img className="skills-img" src="/images/skills/javascript.png" alt="javascript"></img>
-                            <p className="mobile-resp">Javascript</p>
-                            <p className="skills-info">Javascript</p>
-                        </div>
-                        <div className="skills-wrap">
-                            <img className="skills-img" src="/images/skills/react.png" alt="react"></img>
-                            <p className="mobile-resp">React</p>
-                            <p className="skills-info">React</p>
-                        </div>
-                        <div className="skills-wrap">
-                            <img className="skills-img" src="/images/skills/redux.png" alt="redux"></img>
-                            <p className="mobile-resp">Redux</p>
-                            <p className="skills-info">Redux</p>
-                        </div>
-                    </div>
-
-                    <br />
-                    <br />
-                    <div className="skills-row">
-                        <div className="skills-wrap">
-                            <img className="skills-img" src="/images/skills/java.png" alt="java"></img>
-                            <p className="mobile-resp">Java</p>
-                            <p className="skills-info">Java</p>
-                        </div>
-                        <div className="skills-wrap">
-                            <img className="skills-img" src="/images/skills/psql.png" alt="psql"></img>
-                            <p className="mobile-resp">PostgreSqL</p>
-                            <p className="skills-info">PostgreSQL</p>
-                        </div>
-                        <div className="skills-wrap">
-                            <img className="skills-img" src="/images/skills/html.png" alt="html"></img>
-                            <p className="mobile-resp">HTML</p>
-                            <p className="skills-info">HTML</p>
+                    <div id='frontend'>
+                        <div className="skills-pro">
+                            <div className="skills-header-pro">
+                                Frontend
+                            </div>
+                            <br />
+                            <div className="skills-list">
+                                <div className="skills-wrap">
+                                    <img className="skills-img" src="/images/skills/javascript.png" alt="javascript"></img>
+                                    <p className="mobile-resp">Javascript</p>
+                                </div>
+                                <div className="skills-wrap">
+                                    <img className="skills-img" src="/images/skills/react.png" alt="react"></img>
+                                    <p className="mobile-resp">React</p>
+                                </div>
+                                <div className="skills-wrap">
+                                    <img className="skills-img" src="/images/skills/redux.png" alt="redux"></img>
+                                    <p className="mobile-resp">Redux</p>
+                                </div>
+                                <div className="skills-wrap">
+                                    <img className="skills-img" src="/images/skills/html.png" alt="html"></img>
+                                    <p className="mobile-resp">HTML</p>
+                                </div>
+                                <div className="skills-wrap">
+                                    <img className="skills-img" src="/images/skills/css.png" alt="css"></img>
+                                    <p className="mobile-resp">CSS</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
-                    <br />
+
                     <br />
 
-                    <div className="skills-row">
-                        <div className="skills-wrap">
-                            <img className="skills-img" src="/images/skills/css.png" alt="css"></img>
-                            <p className="mobile-resp">CSS</p>
-                            <p className="skills-info">CSS</p>
-                        </div>
-                        <div className="skills-wrap">
-                            <img className="skills-img" src="/images/skills/git.png" alt="git"></img>
-                            <p className="mobile-resp">git</p>
-                            <p className="skills-info">git</p>
-                        </div>
-                        <div className="skills-wrap">
-                            <img className="skills-img" src="/images/skills/node.png" alt="node"></img>
-                            <p className="mobile-resp">Node.js</p>
-                            <p className="skills-info">Node.js</p>
+                    <div id="backend">
+                        <div className="skills-pro">
+                            <div className="skills-header-pro">
+                                Backend
+                            </div>
+                            <br />
+                            <div className="skills-list">
+                                <div className="skills-wrap">
+                                    <img className="skills-img" src="/images/skills/psql.png" alt="psql"></img>
+                                    <p className="mobile-resp">PostgreSQL</p>
+                                </div>
+                                <div className="skills-wrap">
+                                    <img className="skills-img" src="/images/skills/node.png" alt="node"></img>
+                                    <p className="mobile-resp">Node.js</p>
+                                </div>
+                                <div className="skills-wrap">
+                                    <img className="skills-img" src="/images/skills/git.png" alt="git"></img>
+                                    <p className="mobile-resp">git</p>
+                                </div>
+                                <div className="skills-wrap">
+                                        <img className="skills-img" src="/images/skills/java.png" alt="java"></img>
+                                        <p className="mobile-resp">Java</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         )
